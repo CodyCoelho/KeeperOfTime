ef778c3aac36fa194e01a03ec266e821b89f587a
100136905 <100136905@ba-a222-19.student.georgianc.on.ca>
2015-04-15 13:54:44 -0400

    Initial Commit

---
+++ KeeperOfTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeeperOfTime
+//
+//  Created by 100136905 on 2015-04-15.
+//  Copyright (c) 2015 100136905. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
